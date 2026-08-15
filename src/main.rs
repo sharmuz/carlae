6f19736f616c311a9b59c7d5db1ee385e3ec7f64
Muzher Sharif <muzher@gmail.com>
2026-08-15 11:42:23 -0400

    Comment expected token types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// enum TOKEN
+
+// NEWLINE
+// INDENT
+// DEDENT
+
+// identifiers + keywords
+// NAME
+
+// Literals
+// NUMBER
+// STRING
+// BYTES
+
+// Operators + delimeters
+// OP
+// LPAR
+// RPAR
+// PLUS
+// MINUS
+// STAR
+// SLASH
+// EQUAL
+
+// fn tokenize
+
+// fn parse
+
+// fn eval
