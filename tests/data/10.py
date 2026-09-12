e0f09f6a13b15ff963b88e07685583b909b9e02d
Muzher Sharif <muzher@gmail.com>
2026-09-12 11:56:10 -0400

    Add test script for eval of large expr

---
+++ tests/data/10.py
@@ -0,0 +1,16 @@
+# Expected result: CarlaeCarlae!!!!
+(
+    ("Car" + "lae")
+    * (
+        ((8 - True * 6) == 2)
+        + (9 / 3 >= False + 2)
+    )
+    + "!"
+    * (
+        ("cat" > "car")
+        + ("a" < "b")
+        + (True != False)
+        + (2 <= True + True)
+    )
+    + (False * "unused")
+)
